d1163f6fdb090b0a43adb7af55285d686e9dfcfb
sourabhbishnoi <59406258+sourabhbishnoi@users.noreply.github.com>
2020-07-13 10:22:15 +0530

    add addItem and removeItem functionality to our checkout-item component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
 import {selectCartItems, selectCartTotal} from '../../redux/cart/cart.selectors';
-
+import CheckOutItem from '../../components/checkout-item/checkout-item.component';
 import './checkout.styles.scss';
 
 const CheckoutPage = ({cartItems,total}) => (
@@ -26,7 +26,7 @@ const CheckoutPage = ({cartItems,total}) => (
    </div> 
    {
    	cartItems.map(cartItem=>
-   	cartItem.name)
+   	<CheckOutItem key={cartItem.id} cartItem={cartItem} />)
    }
    <div className='total'>
    <span>TOTAL: ${total}</span>
